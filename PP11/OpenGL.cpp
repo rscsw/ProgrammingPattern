d453a1467dde6ad2fbb2951929cd70899d0cbad5
rscsw <cathy29@naver.com>
2022-05-21 18:00:26 +0900

    OpenGL 충돌 처리

--- PP11/OpenGL.cpp
+++ PP11/OpenGL.cpp
@@ -2,39 +2,77 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
 #include <Windows.h>
 
 #pragma comment(lib, "OpenGL32")
 
-namespace MuSeoun
-{
-    struct vec2
-    {
-        float x;
-        float y;
+using namespace std;
 
-        vec2(int x_, int y_)
-        {
-            x = x_;
-            y = y_;
-        }
-    };
-    vec2 operator+(vec2 a, vec2 b)
+struct vec2
+{
+    float x;
+    float y;
+
+    vec2(int x_, int y_)
     {
-        vec2 result(a.x + b.x, a.y + b.y);
-        return result;
+        x = x_;
+        y = y_;
     }
+};
+vec2 operator+(vec2 a, vec2 b)
+{
+    vec2 result(a.x + b.x, a.y + b.y);
+    return result;
 }
+
+class MyRectangle
+{
+public:
+    float x;
+    float y;
+    void DrawRectangle(float r, float g, float b, float a, float xp, float yp)
+    {
+        glBegin(GL_TRIANGLE_STRIP);
+        glColor4f(r, g, b, a);
+        glVertex2f(x, y + yp);
+        glVertex2f(x + xp, y + yp);
+        glVertex2f(x, y);
+        glVertex2f(x + xp, y);
+        glEnd();
+    }
+    void Move()
+    {
+        if (GetAsyncKeyState(VK_LEFT) & 0x8000 || GetAsyncKeyState(VK_LEFT) & 0x8001)
+        {
+            x -= 0.00075f;
+        }
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8000 || GetAsyncKeyState(VK_RIGHT) & 0x8001)
+        {
+            x += 0.00075f;
+        }
+        if (GetAsyncKeyState(VK_UP) & 0x8000 || GetAsyncKeyState(VK_UP) & 0x8001)
+        {
+            y += 0.00075f;
+        }
+        if (GetAsyncKeyState(VK_DOWN) & 0x8000 || GetAsyncKeyState(VK_DOWN) & 0x8001)
+        {
+            y -= 0.00075f;
+        }
+    }
+};
 
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
 }
+
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
 int main(void)
 {
     GLFWwindow* window;
@@ -50,8 +88,13 @@ int main(void)
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
 
-    float x = -1;
-    float y = -1;
+    MyRectangle* player = new MyRectangle();
+    MyRectangle* monster = new MyRectangle();
+
+    player->x = 0.0f;
+    player->y = 0.0f;
+    monster->x = 0.0f;
+    monster->y = 0.0f;
 
     while (!glfwWindowShouldClose(window))
     {
@@ -60,42 +103,19 @@ int main(void)
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float)height;
 
-        if (GetAsyncKeyState(VK_LEFT) & 0x8000 || GetAsyncKeyState(VK_LEFT) & 0x8001)
-        {
-            x -= 0.0005f;
-        }
-        if (GetAsyncKeyState(VK_RIGHT) & 0x8000 || GetAsyncKeyState(VK_RIGHT) & 0x8001)
-        {
-            x += 0.0005f;
-        }
-        if (GetAsyncKeyState(VK_UP) & 0x8000 || GetAsyncKeyState(VK_UP) & 0x8001)
-        {
-            y += 0.0005f;
-        }
-        if (GetAsyncKeyState(VK_DOWN) & 0x8000 || GetAsyncKeyState(VK_DOWN) & 0x8001)
-        {
-            y -= 0.0005f;
-        }
-
-        glClearColor(1.0f, 1.0f, 1.0f, 1);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //사각형 만들기
-        glBegin(GL_TRIANGLE_STRIP);
-        glColor4f(0.0f, 0.0f, 1.0f, 0.5f);
-        glVertex2f(x, y+0.4f);
-        glVertex2f(x+0.4f, y+0.4f);
-        glVertex2f(x, y);
-        glVertex2f(x+0.4f, y);
-        glEnd();
+        player->DrawRectangle(0.0f, 0.0f, 1.0f, 1.0f, 0.3f, 0.3f);
+        monster->DrawRectangle(1.0f, 0.0f, 0.0f, 1.0f, 0.4f, 0.4f);
+        
+        player->Move();
 
-        glBegin(GL_TRIANGLE_STRIP);
-        glColor4f(0.0f, 1.0f, 0.0f, 0.1f);
-        glVertex2f(x, y + 0.4f);
-        glVertex2f(x + 0.4f, y + 0.4f);
-        glVertex2f(x, y);
-        glVertex2f(x + 0.4f, y);
-        glEnd();
+        //충돌 처리
+        if (player->x >= monster->x - 0.3f && player->x + 0.3f <= monster->x + 0.7f && player->y <= monster->y + 0.4f && player->y >= monster->y - 0.3f)
+        {
+            cout << "충돌 발생" << endl;
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
